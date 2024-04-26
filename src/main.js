c223f4f5984254343e4299b2443693fc13d8625e
Peter Bagnall <peter.bagnall@ul.com>
2024-04-26 12:13:39 +0100

    refactor(Label): :lipstick: renamed themes throughout code. Now using resizable in the labels.
    Label demo now uses the resizable container. Last demo has switches rather than repeating content.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 import '../components/Label.js';
-import '../components/Breadcrumbs.js';
 import '../components/Resizable.js';
+
+function switchTheme(event) { 
+   let theme = event.target.id;
+   // Array.from(document.querySelectorAll('main section')).forEach(element => {
+   //    element.className = theme;
+   // });
+   document.getElementsByTagName('body')[0].className = theme;
+
+   let images = document.querySelectorAll('img');
+   for (let image of images) {
+      image.style.display = image.src.indexOf(theme)>=0 ? 'block' : 'none';
+   }
+}
+
+addEventListener('load', () => {
+   document.getElementById('masterbrand').addEventListener('change', (e) => switchTheme(e));
+   document.getElementById('ultrus').addEventListener('change', (e) => switchTheme(e));
+});
